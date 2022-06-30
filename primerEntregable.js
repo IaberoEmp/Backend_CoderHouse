712f24a2657e1671bb62dc3f7bb22e0dd83ec1bf
Ismael Abero <iafemprende@gmail.com>
2022-06-30 18:05:08 -0300

    primer entregable v3 - Funcionando

--- primerEntregable.js
+++ primerEntregable.js
@@ -6,32 +6,32 @@ class Usuario {
         this.petss=[];
     }
     getfullname = function(){
-       console.log(`Hola, mi nombre es ${this.name} ${this.lastname}. Un gusto saludarte.`);
+       console.log(`Hola, mi nombre es ${this.name} ${this.lastname}. Un gusto saludarte.`)
     }
     addpet = function(pet){
         this.petss.push(pet);
     }
     countpets=function(){
-        console.log(`Tengo ${this.petss.lenght} mascotas`);
+        console.log(`Tengo ${this.petss.length} mascotas`);
     }
     addbook=function(title,author){
         this.books.push({Title:title, Author:author});
     }
     getbooknames=function(){
-        let titles = this.books.map(function(title){
-            return `${title.book}`;
+        let names = this.books.map(function(name){
+            return `${name.Title}`;
         })
-        console.log(`Mis libros favoritos son ${titles}`)
+        console.log(`Mis libros favoritos son ${names}`)
     }
 }
 
 const usuario1 = new Usuario("Ismael","Abero");
 
-usuario1.addpet("Perro - Rocky");
-usuario1.addpet("Gato - Pelusa");
-
 usuario1.addbook("El hombre que calcuba","Malba Tahan");
 usuario1.addbook("Agilmente","Estanislao Bachrach");
+
+usuario1.addpet("Perro - Rocky");
+usuario1.addpet("Gato - Pelusa");
 
 usuario1.getfullname();
 usuario1.countpets();
